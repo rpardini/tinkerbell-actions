e3e4daa2113a0713660ceead1afd1354fbb8b70a
Ricardo Pardini <ricardo@pardini.net>
2024-03-19 23:39:18 +0100

    Hack grub2bios

--- grub2disk/main.go
+++ grub2disk/main.go
@@ -15,8 +15,11 @@ func main() {
 	grubBlockDevice := os.Getenv("GRUB_DISK")
 	filesystemType := os.Getenv("FS_TYPE")
 
-	if _, err := exec.Command("/bin/sh", "-c", "apk add grub grub-bios").Output(); err != nil {
-		log.Fatal(fmt.Errorf("failed to install grub-install with error %w", err))
+	if _, err := exec.Command("/bin/sh", "-c", "apk add grub").Output(); err != nil {
+		log.Fatal(fmt.Errorf("failed to install grub with error %w", err))
+	}
+	if _, err := exec.Command("/bin/sh", "-c", "apk add grub-bios").Output(); err != nil {
+		log.Fatal(fmt.Errorf("failed to install grub-bios with error %w", err))
 	}
 	if err := grub.MountGrub(grubInstallPath, grubBlockDevice, filesystemType); err != nil {
 		log.Fatal(err)
